cbeb9cce159602c05c5352872ff90f582e2e3d46
Jonathon Hawkins <thehomelesspanda@gmail.com>
2015-01-12 16:51:14 +0000

    faq and home tempaltes

---
+++ faq.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<title>&nbsp;</title>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="faq.css" />
+		<script type="text/javascript" charset="utf-8" src="cordova/cordova.js"></script>
+	</head>
+	<body>
+		<div id="banner"></div>
+		<div id="logo">
+			<img src="images/logo.png">
+		</div>
+		<article class="main">
+			<img src="images/bleft.png" id="lbub">
+			<img src="images/bright.png" id="rbub">
+			<img src="images/bleft.png" id="lbub">
+			<img src="images/bright.png" id="rbub">
+			<img src="images/bleft.png" id="lbub">
+			<img src="images/bright.png" id="rbub">
+			<img src="images/bleft.png" id="lbub">
+			<img src="images/bright.png" id="rbub">
+			<img src="images/bleft.png" id="lbub">
+			<img src="images/bright.png" id="rbub">
+			<img src="images/bleft.png" id="lbub">
+			<img src="images/bright.png" id="rbub">		
+		</article>
+		<input type="submit" name="submit" id="tc1" value="Terms &#38; Conditoins 1" class="buttonStyle"><input type="submit" name="submit" id="tc2" value="Terms &#38; Conditoins 2" class="buttonStyle">
+		<div id="footer"><input type="submit" name="submit" id="Decline" value="Decline" class="buttonStyle"><input type="submit" name="submit" id="Accept" value="Accept" class="buttonStyle"></div>
+	</body>
+</html>
