f33daa12b5a721cd3c41ef273fc660087ea31fd0
Jonathon Hawkins <thehomelesspanda@gmail.com>
2015-01-12 16:00:18 +0000

    day 1 web stuff

---
+++ welcome.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<title>&nbsp;</title>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="welcome.css" />
+		<script type="text/javascript" charset="utf-8" src="cordova/cordova.js"></script>
+	</head>
+	<body>
+		<a href="log_reg.php"><div id="login">
+			<img src="images/log_reg.png">
+		</div></a>
+		<div id="banner">
+		</div>
+		<div id="logo">
+			<img src="images/logo.png">
+		</div>
+		<div id="facebook">
+			<img src="images/face.png">
+		</div>
+		<footer>
+			<a href="faq.php"><img id="faq" src="images/faq.png"></a?>
+			<img id="nhs" src="images/nhs.png">
+		</footer>
+	</body>
+</html>
