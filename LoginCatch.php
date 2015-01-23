44f50b1d9078db3f116abf4fafb610a0121f6804
Jonathon Hawkins <thehomelesspanda@gmail.com>
2015-01-23 16:45:45 +0000

    login update WOOT WOOT

--- LoginCatch.php
+++ LoginCatch.php
@@ -13,101 +13,83 @@ if ($stmt = $dbcon->prepare("SELECT username,password,accountL,confirmation FROM
 		$stmt->fetch();
 if ($form_user != $user)
 {
-	$User = array('Username'=>"Wrong username");
+	$user = array('user'=>"Wrong username");
 	$errorCount = $errorCount + 1;
 }
 else 
 {
-	$User = array('Username'=>"Correct username");
+	$user = array('user'=>"Correct username");
 }
 if (crypt($form_pass, $pass)!= $pass)
 {
-	$Pass = array('Password'=>"Wrong password");
+	$pass = array('pass'=>"Wrong password");
 	$errorCount = $errorCount + 1;
 }
 else 
 {
-	$Pass = array('Password'=>"Correct password");
+	$pass = array('pass'=>"Correct password");
 }
 if ($conf == 0)
 {
-	$conf = array('Conf'=>"Account not confirmed");
+	$conf = array('conf'=>"Account not confirmed");
 }
 else
 {
-	$conf = array('Conf'=>"Account confirmed");
-}
-if (($_SESSION['wrongp']==true) or ($_SESSION['wrongu']==true))
-{
-	$conf = array('Conf'=>"Account confirmed");
+	$conf = array('conf'=>"Account confirmed");
 }
 if($errorCount > 0)
 {	
 	$stmt->close();
 	$dbcon->close();
-	$User = json_encode($User);
-	echo $User;
-	$Pass = json_encode($Pass);
-	echo $Pass;
-	$conf = json_encode($conf);
-	echo $conf;
 	$state = array('success'=>"3");
-	$state = json_encode($state);
-	echo $state;	
-
+	$result = array_merge($user, $pass, $conf, $state);
+	$result = json_encode($result);
+	echo($result);
 }
 else
 {
-if (($form_user == $user) and (crypt($form_pass, $pass)== $pass))
+	$_SESSION['login']=true;
+	$_SESSION['currentUser']=$form_user;
+	$_SESSION['timeStamp']=time();
+	$_SESSION['currentUser'] = $user;
+	//this is just for expantion so that i can set levels for the users, ie admin/mod/user
+	if ($accountl == 0)
 	{
-		$_SESSION['login']=true;
-		$_SESSION['currentUser']=$form_user;
-		$_SESSION['timeStamp']=time();
-		$_SESSION['currentUser'] = $user;
-		//this is just for expantion so that i can set levels for the users, ie admin/mod/user
-		if ($accountl == 0)
-		{
-			$_SESSION['mod']=false;
-			$_SESSION['admin']=false;
-			$state = array('success'=>0);
-			$state = json_encode($state);
-			echo $state;		
-		}
-		else if ($accountl == 1)
-		{
-			$_SESSION['mod']=true;
-			$_SESSION['admin']=false;
-			$state = array('success'=>1);
-			$state = json_encode($state);
-			echo $state;		
-		}
-		else if ($accountl == 2)
-		{
-			$_SESSION['mod']=true;
-			$_SESSION['admin']=true;
-			$state = array('success'=>2);
-			$state = json_encode($state);
-			echo $state;		
-		}
-		else
-		{
-			$_SESSION['login']=false;
-			$_SESSION['admin']=false;
-			$_SESSION['mod']=false;
-			$state = array('success'=>$form_user);
-			$state = json_encode($state);
-			echo $state;		
-		}
+		$_SESSION['mod']=false;
+		$_SESSION['admin']=false;
+		$state = array('success'=>0);
+		$result = array_merge($user, $pass, $conf, $state);
+		$result = json_encode($result);
+		echo($result);	
 	}
-else
+	else if ($accountl == 1)
 	{
-		//if the username or passswords dont equal the ones on the table kick them out and set a varibale to tell them later.
+		$_SESSION['mod']=true;
+		$_SESSION['admin']=false;
+		$state = array('success'=>1);
+		$result = array_merge($user, $pass, $conf, $state);
+		$result = json_encode($result);
+		echo($result);
+	}
+	else if ($accountl == 2)
+	{
+		$_SESSION['mod']=true;
+		$_SESSION['admin']=true;
+		$state = array('success'=>2);
+		$result = array_merge($user, $pass, $conf, $state);
+		$result = json_encode($result);
+		echo($result);
+	}
+	else
+	{
 		$_SESSION['login']=false;
+		$_SESSION['admin']=false;
+		$_SESSION['mod']=false;
 		$state = array('success'=>3);
-		$state = json_encode($state);
-		echo $state;	
-		mysqli_close($dbcon);
+		$result = array_merge($user, $pass, $conf, $state);
+		$result = json_encode($result);
+		echo($result);		
 	}
-	$stmt->close();
-	$dbcon->close();}}
+$stmt->close();
+$dbcon->close();}}
 ?>
